efed6c6303628dbc9e83e4b9450324f28e4468d6
Spencer Wood <spencercwood@gmail.com>
2016-11-04 00:06:12 -0600

    Send 501 on all routes

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ app.get('/:volume/:book', sendBook);
 app.get('/:volume/:book/:chapter', sendChapter);
 
 function sendVolume(req, res) {
+  res.sendStatus(501);
 }
 
 function sendBook(req, res) {
+  res.sendStatus(501);
 }
 
 function sendChapter(req, res) {
+  res.sendStatus(501);
 }
 
 app.listen(process.env.PORT || 3000);
