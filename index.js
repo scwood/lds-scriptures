59d69bda4db8d8c91fb8500b587887b08d888b3b
Spencer Wood <swood@qualtrics.com>
2016-11-11 10:50:06 -0700

    Fix error message typo

--- index.js
+++ index.js
@@ -34,14 +34,14 @@ async function sendBook(req, res, next) {
     const volume = await db.get(
       'SELECT * FROM volumes WHERE uri=?', req.params.volume)
     if (!volume) {
-      res.status(404).send({ error: `Volume not found.` });
+      res.status(404).send({ error: 'Volume not found.' });
       return;
     }
     const book = await db.get(
       'SELECT * FROM books WHERE uri=? AND volumeId=?',
       [req.params.book, volume.id])
     if (!book) {
-      res.status(404).send({ error: 'Book not vound in volume.'})
+      res.status(404).send({ error: 'Book not found in volume.'})
     }
     const chapters = await db.all(
       'SELECT * FROM chapters WHERE bookId=?', book.id)
